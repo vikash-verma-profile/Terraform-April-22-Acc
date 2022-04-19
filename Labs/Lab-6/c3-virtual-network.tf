40a65f14212fd2ef2af853acac1184f0d333fe13
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-19 13:05:22 +0530

    first commit

---
+++ Labs/Lab-6/c3-virtual-network.tf
@@ -0,0 +1,39 @@
+#Virtual Network
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "myvnet-1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+}
+
+#create Subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "mysubnet-1"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes       = ["10.0.2.0/24"]
+}
+#Create Public Ip 
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "mypublicip-1"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  allocation_method   = "Static"
+  domain_name_label   = "app1-vm-1454"
+  tags = {
+    environment = "Dev"
+  }
+}
+#create Network Intergface
+resource "azurerm_network_interface" "myvmnic" {
+  name                = "vmnic"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mypublicip.id
+  }
+}
---
+++ Labs/Lab-6/c4-linux-virtual-machine.tf
@@ -0,0 +1,23 @@
+resource "azurerm_linux_virtual_machine" "mylinuxvm" {
+  name = "mylinuxvm-1"
+  computer_name = "devlinux-vm1" #hostname of the vm
+  resource_group_name = azurerm_resource_group.myrg.name
+  location = azurerm_resource_group.myrg.location
+  size = "Standard_DS1_V2"
+  disable_password_authentication=false
+  admin_username = "azureuser"
+  network_interface_ids = [azurerm_network_interface.myvmnic.id]
+  admin_password = "Levelup@007"
+  os_disk {
+    name="osdisk"
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "RedHat"
+    offer = "RHEL"
+    sku = "83-gen2"
+    version = "latest"
+  }
+
+}
