15fc8804283bae65c81afe319830aa6bf7cea86f
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-19 15:16:11 +0530

    first commit

--- Labs/Lab-7/c3-virtual-network.tf
+++ Labs/Lab-7/c3-virtual-network.tf
@@ -1,6 +1,6 @@
 #Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet-1"
+  name                = "myvnet-2"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "myvnet" {
 
 #create Subnet
 resource "azurerm_subnet" "mysubnet" {
-  name                 = "mysubnet-1"
+  name                 = "mysubnet-2"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -29,7 +29,7 @@ resource "azurerm_public_ip" "mypublicip" {
 }
 #create Network Intergface
 resource "azurerm_network_interface" "myvmnic" {
-  name                = "vmnic"
+  name                = "vmnic-1"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 
