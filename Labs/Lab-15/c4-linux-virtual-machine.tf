e86a85c0551c7429436f396a8ee8088d7a0d6025
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-21 10:54:40 +0530

    first commit

---
+++ Labs/Lab-15/c4-linux-virtual-machine.tf
@@ -0,0 +1,35 @@
+resource "azurerm_linux_virtual_machine" "mylinuxvm" {
+  name = "mylinuxvm-2"
+  computer_name = "devlinux-vm1" #hostname of the vm
+  resource_group_name = azurerm_resource_group.myrg.name
+  location = azurerm_resource_group.myrg.location
+  size = "Standard_DS1_v2"
+  disable_password_authentication=false
+  admin_username = "azureuser"
+  network_interface_ids = [azurerm_network_interface.myvmnic.id]
+  admin_password = "Levelup@007"
+  os_disk {
+    name="osdisk"
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "RedHat"
+    offer = "RHEL"
+    sku = "83-gen2"
+    version = "latest"
+  }
+
+  connection {
+    type="ssh"
+    host=self.public_ip_address
+    user=self.admin_username
+    password = self.admin_password
+  }
+  #file provisioner
+  provisioner "file" {
+    source="apps/index.html"
+    destination = "/tmp/index.html"
+  }
+
+}
